f5e70607f7441335f7fcf369b04c03b9e7bb5613
MIRZOULUGBEK9008 <ulugbekkhudoyberdiyev9008@gmail.com>
2023-03-29 13:41:07 +0500

    Fix problem without "greaterColor"

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 const elLoader = document.querySelector(".loader-wrapper"),
 elHome = document.querySelector(".js-score-home"),
 elGuest = document.querySelector(".js-score-guest"),
-elScores = document.querySelectorAll(".js-button");
+elScores = document.querySelectorAll(".js-button"),
+elReset = document.querySelector(".reset");
 
 // LOADER
 document.addEventListener("DOMContentLoaded", function () {
@@ -10,34 +11,42 @@ document.addEventListener("DOMContentLoaded", function () {
   }, 1000);
 });
 
-const greater = "greater";
+const greaterColor = "greater-color";
 
-let scores = [0, 0];
+let scoresHome = 0;
+let scoresGuest = 0;
 
 // checkGreater() function
 function checkGreater() {
-  if (scores[0] > scores[1]) {
-    scores[0].classList.add("greater")
-    scores[1].classList.toggle("greater")
-  } else if (scores[0] < scores[1]) {
-    scores[1].classList.add("greater")
-    scores[0].classList.remove("greater")
+  if (scoresHome > scoresGuest) {
+    scoresHome.classList.add(greaterColor);
+    scoresGuest.classList.remove(greaterColor);
+  } else if (scoresHome < scoresGuest) {
+    scoresGuest.classList.add(greaterColor);
+    scoresHome.classList.remove(greaterColor);
   } else {
-    scores[0].classList.remove("greater")
-    scores[1].classList.remove("greater")
+    scoresHome.classList.remove(greaterColor);
+    scoresGuest.classList.remove(greaterColor);
   }
 }
 
 elScores.forEach(score => {
   score.addEventListener("click", () => {
     if (score.dataset.player === "0") {
-      scores[0] =+ score.value;
-      elHome.textContent = scores[0];
+      scoresHome += score.value * 1;
+      elHome.textContent = scoresHome;
       checkGreater();
     } else {
-      scores[1] =+ score.value;
-      elGuest.textContent = scores[1];
+      scoresGuest += score.value * 1;
+      elGuest.textContent = scoresGuest;
       checkGreater();
     }
   });
+});
+
+elReset.addEventListener("click", () => {
+  scoresHome = 0;
+  scoresGuest = 0;
+  elHome.textContent = scoresHome;
+  elGuest.textContent = scoresGuest;
 });
